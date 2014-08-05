a951fb848489b65b380a8bdcb6ccd00df7d4ebe4
goni <goni1986@sina.com>
2014-08-05 18:40:14 +0800

    commit shop

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,7 +4,14 @@ return array(
 
     //数据库类型://用户名:密码@数据库地址:数据库端口/数据库名#字符集
     'DB_DSN'        => 'mysql://root:admin@localhost:3306/self-drug#utf8',
+
     'URL_MODEL'     => 0,
+
+    //表单令牌验证相关的配置参数
+    'TOKEN_ON'      =>    true,  // 是否开启令牌验证 默认关闭
+    'TOKEN_NAME'    =>    '__hash__',    // 令牌验证的表单隐藏字段名称，默认为__hash__
+    'TOKEN_TYPE'    =>    'md5',  //令牌哈希验证规则 默认为MD5
+    'TOKEN_RESET'   =>    true,  //令牌验证出错后是否重置令牌 默认为true
 
     //域名
     'DOMAIN'        => array(
--- Application/Home/Controller/ShopController.class.php
+++ Application/Home/Controller/ShopController.class.php
@@ -2,20 +2,61 @@
 namespace Home\Controller;
 use Think\Controller;
 class ShopController extends Controller {
-    //public function _initialize(){
-    public function _before_index(){
+    public function _initialize(){
         $is_login =A('Common/Account')->is_login();
         if($is_login !== true){
+            if(IS_AJAX){
+                $this->ajaxReturn(array('errno' => 9999, 'errmsg' => '未登录'));
+            }
             $this->error("未登录", U('Login/index'), 3);
         }
     }
     public function index(){
-        $sql = "SELECT * FROM `shop` WHERE `account_id` = {$_SESSION['account']['id']} AND `status` IN (2,3)";
-        $list = D()->query($sql);
+        $map['account_id'] = $_SESSION['account']['id'];
+        $map['status'] = array('in', array(2,3));
+        $count = D('Shop')->where($map)->count();
+        import("@.Library.Page");
+        $Page = new \Home\Library\Page($count,15);
+        $show = $Page->show();
+        $list = D('Shop')->where($map)->order('id DESC')->limit($Page->firstRow.','.$Page->listRows)->select();
         $this->assign('list', $list);
+        $this->assign('page', $show);
         $this->display();
     }
     public function add(){
+        if(IS_POST){
+            //插入数据库
+            $insert_msg = D('Shop')->insert_data($_POST);
+            if($insert_msg['errno'] !== 0){
+                $this->ajaxReturn($insert_msg);
+            }
+            $insert_id = $insert_msg['data']['id'];
+            //图片
+            D('Shop')->insertall_img_data($insert_id,$_POST['img']);
+            $return = array('errno' => 0);
+            $this->ajaxReturn($return);
+        }
+        $this->display();
+    }
+    public function edit(){
+        if(IS_POST){
+            //更新数据库
+            $update_msg = D('Shop')->update_data($_POST);
+            if($update_msg['errno'] !== 0){
+                $this->ajaxReturn($update_msg);
+            }
+            $update_id = $update_msg['data']['id'];
+            //图片
+            D('Shop')->insertall_img_data($update_id,$_POST['img']);
+            $return = array('errno' => 0);
+            $this->ajaxReturn($return);
+        }
+        $res = D('Shop')->find_data();
+        if(!$res){
+            $this->error("访问错误", U('index'), 3);
+        }
+        $res['img_res'] = D('Shop')->findall_img_data($res['id']);
+        $this->assign('res', $res);
         $this->display();
     }
     public function upload(){
